c4225ffdab3daf4f07dc520b430d03f6f493be93
ji1veturky <jeddah.balgame@gmail.com>
2013-05-25 18:59:49 -0600

    Update gw2apiTest.js
    Added examples

--- gw2apiTest.js
+++ gw2apiTest.js
@@ -1,5 +1,33 @@
-    var xhr = new XMLHttpRequest();
+/*    var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api.guildwars2.com/v1/recipes.json", false);
     xhr.send();
     
-   console.log(xhr.recipes);
+   console.log(xhr.recipes);*/
+   
+   /* var jqxhr = $.getJSON("https://api.guildwars2.com/v1/wvw/objective_names.json",function(){
+    console.log("success");
+})
+
+.done(function() { console.log( "second success" ); })
+.fail(function() { console.log( "error" ); })
+.always(function() { console.log( "complete" ); });
+ 
+jqxhr.complete(function() { console.log( "second complete" ); }); */
+
+var GW2API = "https://api.guildwars2.com/v1/event_names.json";
+var maps = [];
+var numEvent=[];
+
+
+	// function majorEvnt(){
+		// $.getJSON(GW2API, function(data){
+			// $.each(data['events'], function(key,val) {
+				// console.log(maps[val['event_id']]);
+			// })
+		// });
+	// } 
+
+//setup document ready for jquery
+$(function(){
+
+})
